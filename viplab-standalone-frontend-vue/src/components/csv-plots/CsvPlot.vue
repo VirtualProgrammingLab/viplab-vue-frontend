2e47386c1c64238a7954338db7d9fb298d4a0867
ac132064 <katharina.goenner@tik.uni-stuttgart.de>
2022-03-15 12:06:30 +0100

    correct small error in csv to fix y-value formatting

--- viplab-standalone-frontend-vue/src/components/csv-plots/CsvPlot.vue
+++ viplab-standalone-frontend-vue/src/components/csv-plots/CsvPlot.vue
@@ -167,7 +167,7 @@ export default {
         let lastIndex = this.csvs[this.fileIndex].lastIndexOf('/');
         let title = ((this.areUrlsProp) ? this.csvs[this.fileIndex].substr(lastIndex + 1, this.csvs[this.fileIndex].length) : "Graph");
         let xformat = (this.labelProp.format) ? this.labelProp.format : ".1f"
-        let yformat = (this.labelProp.format) ? this.datasetProp.format : ".1f"
+        let yformat = (this.datasetProp.format) ? this.datasetProp.format : ".1f"
         this.layout = {
           title: title,
           xaxis: {
